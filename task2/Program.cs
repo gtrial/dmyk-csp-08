7acd0e0622c2b38982cea291538c23a8bce0e3cc
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-06-25 18:41:29 +0300

    Task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 
 namespace task2
 {
@@ -6,7 +7,24 @@ namespace task2
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var myClass = new MyClass(1, 2);
+            var myClassJson = JsonSerializer.Serialize(myClass, myClass.GetType());
+            Console.WriteLine(myClassJson);
+            if (JsonSerializer.Deserialize(myClassJson, typeof(MyClass)) is MyClass myClass2)
+                Console.WriteLine(myClass2.X + " " + myClass2.Y);
+        }
+    }
+
+    [Serializable]
+    internal class MyClass
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public MyClass(int x, int y)
+        {
+            X = x;
+            Y = y;
         }
     }
 }
